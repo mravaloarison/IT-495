ff91f2067b24eef0e039ed361f2ecda86cdb09b8
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-04-03 20:52:11 +0000

    firebase added

---
+++ do_not_touch/app/firebase_utils.ts
@@ -0,0 +1,34 @@
+import { getAuth } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDqOD6L7bshB9-PRQ3UGccvZa_hQKwO0nI",
+    authDomain: "doordashfashion.firebaseapp.com",
+    projectId: "doordashfashion",
+    storageBucket: "doordashfashion.firebasestorage.app",
+    messagingSenderId: "61918887629",
+    appId: "1:61918887629:web:9993e261eb65eddc6ab384"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export const auth = getAuth(app);
+
+export async function addUserToDB(data: any) {
+    const docRef = doc(db, "users", data.email);
+    await setDoc(docRef, data);
+}
+
+export async function isUserAlreadyInDB(email: string) {
+    const docRef = doc(db, "users", email);
+    const docSnap = await getDoc(docRef);
+    return docSnap.exists();
+}
+
+export async function getUserFromDB(email: string) {
+    const docRef = doc(db, "users", email);
+    const docSnap = await getDoc(docRef);
+    return docSnap.data();
+}
