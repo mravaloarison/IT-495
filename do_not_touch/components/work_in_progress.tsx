8518114d2104bdae66f9e80a3d9db757c84cb822
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-04-07 22:18:28 +0000

    centered text for work in progress

--- do_not_touch/components/work_in_progress.tsx
+++ do_not_touch/components/work_in_progress.tsx
@@ -2,7 +2,7 @@ export default function WorkInProgressView() {
 	return (
 		<div className="flex flex-col items-center justify-center h-52">
 			<h1 className="text-2xl font-bold">Work in Progress</h1>
-			<p className="mt-4 text-gray-500">
+			<p className="mt-4 text-gray-500 text-center">
 				This page is currently under construction. Please check back
 				later.
 			</p>
