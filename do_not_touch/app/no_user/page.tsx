4b929719fd8a12431784531490208a9b68aff26e
mravaloarison <100179286+mravaloarison@users.noreply.github.com>
2025-04-08 04:08:43 +0000

    img link in no_user updated

--- do_not_touch/app/no_user/page.tsx
+++ do_not_touch/app/no_user/page.tsx
@@ -73,7 +73,7 @@ export default function NoUser() {
 				{/* Right Visual Section */}
 				<div className="w-full md:w-1/2">
 					<img
-						src="https://images.unsplash.com/photo-1544441893-675973e31985?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+						src="https://images.unsplash.com/photo-1620799140408-edc6dcb6d633?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D"
 						alt="Fashion illustration"
 						className="rounded-xl shadow-lg object-cover w-full h-72 md:h-96"
 					/>
